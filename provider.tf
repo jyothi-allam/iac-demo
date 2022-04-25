b17043759286f713c5a6d3339a61cf0a47f68fba
jyothi-allam <102275341+jyothi-allam@users.noreply.github.com>
2022-04-26 04:40:18 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region     = "ap-southeast-1"
-  access_key = "AKIAQC774R7XORT4I6MD"
-  secret_key = "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"
+  access_key = "AKIAQC774R"
+  secret_key = "4Urb4SRIZAkOJ7xwMsVS5qSi8c"
 }
